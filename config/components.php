9f596262fd26ed17980314abd5d4afd89f9ecb8d
Luis Arce <luilliarcec@gmail.com>
2020-11-29 16:04:19 -0500

    Adding Checkbox Field Component

--- config/components.php
+++ config/components.php
@@ -21,6 +21,7 @@ return [
     \Styde\Form\View\Components\Fields\Selectables\SelectMultiple::class => 'select-multiple',
     \Styde\Form\View\Components\Fields\Selectables\Radio::class => 'radio',
     \Styde\Form\View\Components\Fields\Selectables\RadioMultiple::class => 'radio-multiple',
+    \Styde\Form\View\Components\Fields\Selectables\Checkbox::class => 'checkbox',
     \Styde\Form\Field::class => 'field',
     \Styde\Form\SelectField::class => 'field-select',
     \Styde\Form\CheckboxField::class => 'field-checkbox',
