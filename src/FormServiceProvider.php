ef8a903723aed344586d62e5474615415d7a6f2a
Luis Arce <luilliarcec@gmail.com>
2020-11-29 14:51:31 -0500

    Load translation files

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -13,6 +13,7 @@ class FormServiceProvider extends ServiceProvider
          * Optional methods to load your package assets
          */
         $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'styde-form');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'styde-form');
 
         /*
